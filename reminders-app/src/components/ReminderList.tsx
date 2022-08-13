4ef9959858eebf6505d5f5dbf390f649f74aa843
NchiaEmmanuela <ekeinchiaemmanuela@gmail.com>
2022-08-13 13:07:22 +0100

    components/models created

---
+++ reminders-app/src/components/ReminderList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Reminder from '../models/reminder'
+
+interface ReminderListProps{
+    itmes : Reminder[]
+}
+
+function ReminderList(props: ReminderListProps) {
+  return (
+    <div>ReminderList</div>
+  )
+}
+
+export default ReminderList
